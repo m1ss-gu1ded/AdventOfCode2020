3e888a462f9075bdb8ee864a6f5d46226c7dee37
Miss Guided <krstn.prtrmnn@gmail.com>
2020-12-05 15:29:30 +0100

    Day 1 clean-up

--- day01/report_repair_1.py
+++ day01/report_repair_1.py
@@ -6,8 +6,9 @@ report_list = [int(x.strip()) for x in report_list]
 list_length = len(report_list)
 
 for entry in report_list:
-    for i in range(report_list.index(entry) + 1, list_length):
-        first_num = report_list[report_list.index(entry)]
+    entry_index = report_list.index(entry)
+    for i in range(entry_index + 1, list_length):
+        first_num = report_list[entry_index]
         second_num = report_list[i]
         num_sum = first_num + second_num
         if num_sum == 2020:
--- day01/report_repair_2.py
+++ day01/report_repair_2.py
@@ -5,9 +5,10 @@ report_list = [int(x.strip()) for x in report_list]
 list_length = len(report_list)
 
 for entry in report_list:
-    for i in range(report_list.index(entry) + 1, list_length):
+    entry_index = report_list.index(entry)
+    for i in range(entry_index + 1, list_length):
         for j in range(i + 1, list_length):
-            first_num = report_list[report_list.index(entry)]
+            first_num = report_list[entry_index]
             second_num = report_list[i]
             third_num = report_list[j]
             num_sum = first_num + second_num + third_num
