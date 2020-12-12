c30b6bca2a0dcde4fb13fcf8733d2f3782f85d5d
Miss Guided <krstn.prtrmnn@gmail.com>
2020-12-12 16:39:06 +0100

    Day 12 part 1

---
+++ day12/navigation.py
@@ -0,0 +1,85 @@
+with open("input_12.txt") as file:
+    instr_list = file.readlines()
+instr_list = [x.strip() for x in instr_list]
+
+
+def check_instr(position, orientation, action, value):
+    if action == "N":
+        move_north(position, value)
+    elif action == "S":
+        move_south(position, value)
+    elif action == "E":
+        move_east(position, value)
+    elif action == "W":
+        move_west(position, value)
+    elif action == "L":
+        turn_left(orientation, value)
+    elif action == "R":
+        turn_right(orientation, value)
+    elif action == "F":
+        move_forward(position, orientation, value)
+
+
+def move_north(position, value):
+    position[1] += value
+
+
+def move_south(position, value):
+    position[1] -= value
+
+
+def move_east(position, value):
+    position[0] += value
+
+
+def move_west(position, value):
+    position[0] -= value
+
+
+def turn_left(orientation, value):
+    if value == 90:
+        orientation[0] -= 1
+    elif value == 180:
+        orientation[0] -= 2
+    elif value == 270:
+        orientation[0] -= 3
+    if orientation[0] < 1:
+        orientation[0] += 4
+
+
+def turn_right(orientation, value):
+    if value == 90:
+        orientation[0] += 1
+    elif value == 180:
+        orientation[0] += 2
+    elif value == 270:
+        orientation[0] += 3
+    if orientation[0] > 4:
+        orientation[0] -= 4
+
+
+def move_forward(position, orientation, value):
+    if orientation[0] == 1:
+        move_east(position, value)
+    elif orientation[0] == 2:
+        move_south(position, value)
+    elif orientation[0] == 3:
+        move_west(position, value)
+    elif orientation[0] == 4:
+        move_north(position, value)
+
+
+# pos[0] is east, pos[1] is north
+pos = [0, 0]
+ori = [1]
+
+for line in instr_list:
+    act = line[0]
+    val = int(line[1:])
+
+    check_instr(position=pos, orientation=ori, action=act, value=val)
+    print(pos)
+
+manhattan = abs(pos[0]) + abs(pos[1])
+
+print(f"Manhattan distance between start and end: {manhattan}")
